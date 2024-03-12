e2851ffd1d450ae8f3e1c1072c2bf5757b4ed793
cr0c0 <hyde69ciel@gmail.com>
2024-03-12 23:08:23 +0900

    #240312 북카드 크기 조절

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,24 +1,32 @@
-import { CardTitle } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 
 function BookCard({ book }) {
+  /**
+   * <Card.Body>
+          <Card.Text>
+            {book.description.length > 43
+              ? book.description.substring(0, 43) + "..."
+              : book.description}
+          </Card.Text>
+        </Card.Body>
+   */
   return (
     <Col>
-      <Card style={{ width: "12rem" }}>
-        <Card.Header as="h5">{book.title}</Card.Header>
-        <Card.Img variant="top" src={book.cover} />
-        <Card.Body>
-          <Card.Text>{book.description}</Card.Text>
-        </Card.Body>
+      <Card style={{ width: "15rem" }} className="mb-4">
+        <Card.Header as="h6">
+          {book.title.length > 28
+            ? book.title.substring(0, 28) + "..."
+            : book.title}
+        </Card.Header>
+
+        <Card.Img variant="top" src={book.cover} style={{ height: "20rem" }} />
+
         <ListGroup className="list-group-flush">
           <ListGroup.Item>{book.author}</ListGroup.Item>
           <ListGroup.Item>{book.publisher}</ListGroup.Item>
         </ListGroup>
-        <Card.Body>
-          <Card.Link href={book.link}>알라딘 상품 페이지</Card.Link>
-        </Card.Body>
       </Card>
     </Col>
   );
