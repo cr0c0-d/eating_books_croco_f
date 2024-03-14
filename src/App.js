23a23c3bcbfb22fef0d1cf48dbb2ab508781ec54
cr0c0 <hyde69ciel@gmail.com>
2024-03-14 20:30:31 +0900

    #240314 react router 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SearchBooks from "./components/SearchBooks";
+import SignUpForm from "./member/SignUpForm";
+import Login from "./member/Login";
 import Layout from "./Layout";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
-      children: [{ path: "/search", element: <SearchBooks /> }],
+      children: [
+        { path: "/search", element: <SearchBooks /> },
+        { path: "/signup", element: <SignUpForm /> },
+        { path: "/login", element: <Login /> },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
