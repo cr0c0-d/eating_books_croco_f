c982caa275c70dea133ee8e1ded9c7248d62e0e9
cr0c0 <hyde69ciel@gmail.com>
2024-04-12 10:03:44 +0900

    #240412 로그인 페이지 진입시 토큰 확인

--- src/components/member/Login.js
+++ src/components/member/Login.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 import Button from "react-bootstrap/Button";
@@ -7,11 +7,50 @@ import Container from "react-bootstrap/Container";
 import { useUser } from "./UserContext";
 
 import axios from "axios";
+import AuthAPI from "../../AuthAPI";
 
 function Login() {
   const history = useNavigate();
   const location = useLocation();
-  const { setUserInfo } = useUser();
+  const { userInfo, setUserInfo } = useUser();
+  const [loginStatus, setLoginStatus] = useState(true);
+
+  // 로그인 성공시
+  const loginSuccess = (res) => {
+    if (res !== undefined && (res.status === 201 || res.status === 200)) {
+      setUserInfo({ ...userInfo, accessToken: res.data.accessToken }); // userInfo 업데이트
+      afterLoginRedirect();
+    }
+  };
+
+  // 페이지 이동
+  const afterLoginRedirect = () => {
+    console.log(location.state);
+    if (location.state && location.state.beforeUrl) {
+      // 이전 페이지 기록이 있으면
+      history(location.state.beforeUrl, { state: { ...location.state } });
+    } else {
+      // 기록이 없으면
+      history("/search");
+    }
+  };
+
+  // 로그인 전 토큰 확인
+  const chkLoginStatus = () => {
+    AuthAPI({
+      url: "/api/token",
+      method: "POST",
+      data: null,
+      success: loginSuccess,
+      fail: () => {
+        setLoginStatus(false);
+      },
+    });
+  };
+
+  useEffect(() => {
+    chkLoginStatus();
+  }, []);
 
   const [formData, setFormData] = useState({
     email: "",
@@ -91,78 +130,76 @@ function Login() {
         role: response.data.role,
       });
 
-      if (location.state && location.state.beforeUrl) {
-        // 이전 페이지 기록이 있으면
-        history(location.state.beforeUrl, { state: { ...location.state } });
-      } else {
-        // 기록이 없으면
-        history("/search");
-      }
+      afterLoginRedirect();
     }
   };
 
   return (
     <div>
-      <Container>
-        <div>
-          <h1 className="mt-4 mb-4">로그인</h1>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>이메일</Form.Label>
-              <Form.Control
-                value={formData.email}
-                name="email"
-                onChange={handleChange}
-                type="email"
-                placeholder="이메일을 입력하세요."
-              />
-              {errors.email && (
-                <Form.Text id="email" style={{ color: "red" }}>
-                  {errors.email}
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>비밀번호</Form.Label>
-              <Form.Control
-                value={formData.password}
-                name="password"
-                onChange={handleChange}
-                type="password"
-                placeholder="비밀번호를 입력하세요."
-              />
-              {errors.password && (
-                <Form.Text id="email" style={{ color: "red" }}>
-                  {errors.password}
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            {errors.loginFail && (
-              <Form.Group className="mb-3">
-                <Form.Text id="email" style={{ color: "red" }}>
-                  {errors.loginFail}
-                </Form.Text>
+      {!loginStatus ? (
+        <Container>
+          <div>
+            <h1 className="mt-4 mb-4">로그인</h1>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>이메일</Form.Label>
+                <Form.Control
+                  value={formData.email}
+                  name="email"
+                  onChange={handleChange}
+                  type="email"
+                  placeholder="이메일을 입력하세요."
+                />
+                {errors.email && (
+                  <Form.Text id="email" style={{ color: "red" }}>
+                    {errors.email}
+                  </Form.Text>
+                )}
               </Form.Group>
-            )}
 
-            <Button variant="primary" type="submit" onClick={onSubmit}>
-              로그인
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>비밀번호</Form.Label>
+                <Form.Control
+                  value={formData.password}
+                  name="password"
+                  onChange={handleChange}
+                  type="password"
+                  placeholder="비밀번호를 입력하세요."
+                />
+                {errors.password && (
+                  <Form.Text id="email" style={{ color: "red" }}>
+                    {errors.password}
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              {errors.loginFail && (
+                <Form.Group className="mb-3">
+                  <Form.Text id="email" style={{ color: "red" }}>
+                    {errors.loginFail}
+                  </Form.Text>
+                </Form.Group>
+              )}
+
+              <Button variant="primary" type="submit" onClick={onSubmit}>
+                로그인
+              </Button>
+            </Form>
+          </div>
+          <div>
+            <Button href="/signup">회원가입</Button>
+          </div>
+          <div>
+            <Button
+              href={`${process.env.REACT_APP_API_ROOT}/oauth2/authorization/google`}
+            >
+              구글 로그인
             </Button>
-          </Form>
-        </div>
-        <div>
-          <Button href="/signup">회원가입</Button>
-        </div>
-        <div>
-          <Button
-            href={`${process.env.REACT_APP_API_ROOT}/oauth2/authorization/google`}
-          >
-            구글 로그인
-          </Button>
-        </div>
-      </Container>
+          </div>
+        </Container>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
