02563436f9df08069d535b93f14665bb4430f5f3
cr0c0 <hyde69ciel@gmail.com>
2024-04-13 14:19:01 +0900

    #240413 회원정보 수정 페이지, 이미지 업로드

--- src/components/member/Member.js
+++ src/components/member/Member.js
@@ -7,9 +7,12 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
+import FileUpload from "../../FileUpload";
+import axios from "axios";
 
 function Member() {
   const [memberInfo, setMemberInfo] = useState(null);
+  const [profileImg, setProfileImg] = useState("");
   const [editNickname, setEditNickname] = useState(false);
 
   const history = useNavigate();
@@ -21,11 +24,15 @@ function Member() {
       success: (response) => {
         console.log("성공");
         setMemberInfo({ ...response.data, password: "" });
+        setProfileImg(response.data.profileImg);
       },
       fail: (err) => {
-        if (err.response.status == 403) {
-          alert("권한이 없습니다.");
-          history(-1);
+        if (err) {
+          console.log(err);
+          if (err.response.status == 403) {
+            alert("권한이 없습니다.");
+            history(-1);
+          }
         }
       },
     });
@@ -33,64 +40,125 @@ function Member() {
   useEffect(() => {
     findMember();
   }, []);
+
+  // 이미지 업로드 imgbb 이용
+  const imgUpload = async (file) => {
+    const date = Date.now();
+
+    const axiosResponse = await axios({
+      url: "https://api.imgbb.com/1/upload",
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: {
+        key: `${process.env.REACT_APP_IMGBB_KEY}`,
+        image: file,
+        name: "member" + date.valueOf(),
+      },
+    }).catch((err) => {
+      console.log("실패");
+      console.log(err);
+    });
+    if (axiosResponse) {
+      console.log(axiosResponse);
+      setMemberInfo({
+        ...memberInfo,
+        profileImg: axiosResponse.data.data.url,
+      });
+    }
+  };
+
+  const updateMember = () => {
+    AuthAPI({
+      url: "/api/members",
+      method: "PUT",
+      data: memberInfo,
+      success: (response) => {
+        console.log("성공");
+        window.location.reload();
+      },
+      fail: (err) => {
+        if (err) {
+          console.log(err);
+        }
+      },
+    });
+  };
+
   return (
     <div>
       {memberInfo ? (
-        <Form>
-          <Row className="justify-content-md-center">
-            <Col xs={3} md={3}>
-              <Image
-                fluid
-                src="https://img1.daumcdn.net/thumb/C428x428/?scode=mtistory2&fname=https%3A%2F%2Ftistory4.daumcdn.net%2Ftistory%2F6777467%2Fattach%2F62d6465866e64df897d893fa9d46ea8e"
-                roundedCircle
-              />
-              <Form.Control type="file" />
-            </Col>
-          </Row>
-          <br />
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm="2">
-              이메일
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control
-                type="email"
-                value={memberInfo.email}
-                readOnly
-                plaintext
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm="2">
-              비밀번호
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control
-                type="password"
-                value={memberInfo.password}
-                onChange={(e) => {
-                  setMemberInfo({ ...memberInfo, password: e.target.value });
-                }}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm="2">
-              닉네임
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control
-                type="text"
-                value={memberInfo.nickname}
-                onChange={(e) => {
-                  setMemberInfo({ ...memberInfo, nickname: e.target.value });
-                }}
-              />
-            </Col>
-          </Form.Group>
-          <Button>저장</Button>
-        </Form>
+        <div>
+          <h1 className="mt-4 mb-4">정보 수정</h1>
+          <Form>
+            <Row className="justify-content-md-center">
+              <Col xs={3} md={3}>
+                <Image fluid src={profileImg} roundedCircle />
+              </Col>
+              <Row className="justify-content-md-center">
+                <Col xs={4} md={4}>
+                  <Form.Control
+                    type="text"
+                    value={memberInfo.nickname}
+                    onChange={(e) => {
+                      setMemberInfo({
+                        ...memberInfo,
+                        nickname: e.target.value,
+                      });
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => {
+                    //FileUpload({ file: e.target.files[0], type: "members" });
+                    // setMemberInfo({
+                    //   ...memberInfo,
+                    //   profileImg: e.target.files[0],
+                    // });
+                    imgUpload(e.target.files[0]);
+                    const imgUrl = URL.createObjectURL(e.target.files[0]);
+                    setProfileImg(imgUrl);
+                  }}
+                />
+              </Row>
+            </Row>
+
+            <br />
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                이메일
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control
+                  type="email"
+                  value={memberInfo.email}
+                  readOnly
+                  plaintext
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                비밀번호
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control
+                  type="password"
+                  value={memberInfo.password}
+                  onChange={(e) => {
+                    setMemberInfo({ ...memberInfo, password: e.target.value });
+                  }}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3"></Form.Group>
+            <Button onClick={() => updateMember()}>저장</Button>
+          </Form>
+        </div>
       ) : (
         ""
       )}
