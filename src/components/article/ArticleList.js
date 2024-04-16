7ed7170da7fc7ce28f31387570bf140c3115b6d4
cr0c0 <hyde69ciel@gmail.com>
2024-04-16 23:44:55 +0900

    #240416 글 목록 - 조회된 글이 없을 시 메세지 추가

--- src/components/article/ArticleList.js
+++ src/components/article/ArticleList.js
@@ -23,30 +23,40 @@ function ArticleList({ articleList }) {
             <Col md="2">작성일시</Col>
           </Row>
         </ListGroup.Item>
-        {articleList
-          ? articleList.map((article) => (
-              <ListGroup.Item key={article.id}>
-                <Row>
-                  <Col md="1">
-                    {article.articleType == "B" ? "식전문" : "식후문"}
-                  </Col>
-                  <Col md="5" className="ellipsis">
-                    <span
-                      onClick={() => history(`/articles/${article.id}`)}
-                      style={{ cursor: "pointer" }}
-                    >
-                      {article.title}
-                    </span>
-                  </Col>
-                  <Col md="1">{article.writerNickname}</Col>
-                  <Col md="3" className="ellipsis">
-                    {article.bookTitle}
-                  </Col>
-                  <Col md="2">{article.createdAt}</Col>
-                </Row>
-              </ListGroup.Item>
-            ))
-          : ""}
+        {articleList && articleList.length > 0 ? (
+          articleList.map((article) => (
+            <ListGroup.Item key={article.id}>
+              <Row>
+                <Col md="1">
+                  {article.articleType == "B" ? "식전문" : "식후문"}
+                </Col>
+                <Col md="5" className="ellipsis">
+                  <span
+                    onClick={() => history(`/articles/${article.id}`)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {article.title}
+                  </span>
+                </Col>
+                <Col md="1" className="ellipsis">
+                  {article.writerNickname}
+                </Col>
+                <Col md="3" className="ellipsis">
+                  {article.bookTitle}
+                </Col>
+                <Col md="2">{article.createdAt}</Col>
+              </Row>
+            </ListGroup.Item>
+          ))
+        ) : (
+          <ListGroup.Item>
+            <Row className="justify-content-md-center">
+              <Col style={{ textAlign: "center" }}>
+                <span>등록된 글이 없습니다.</span>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        )}
       </ListGroup>
     </div>
   );
